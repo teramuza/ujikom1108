bde0af17495b0367f41f72792d3a220c1e005ee6
Teuku Raja <tera@flip.id>
2025-08-11 15:09:11 +0700

    dist

---
+++ dist/config/sequelize.config.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+const config = {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+};
+if (process.env.DEV_MODE === 'false') {
+    config.use_env_variable = 'DATABASE_URL';
+    config.logging = false;
+    config.dialectOptions = {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    };
+}
+exports.default = config;
